55511e9aacea35f08f694d37c5e93642c59458d4
YWZHONG <mscboom@gmail.com>
2022-09-15 16:29:11 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+PLATFORM = "Android"
+DEVICE_NAME = "安卓设备名称"
+APP_PACKAGE = "com.tencent.mm"
+APP_ACTIVITY = ".ui.LauncherUI"
+NORESET = True
+DRIVER_SERVER = "http://127.0.0.1:4723/wd/hub"
+TIMEOUT = 600
+
+FLICK_START_X = 300
+FLICK_START_Y = 300
+FLICK_START_X = 300
+FLICK_START_Y = 300
+
+
+SCROLL_SLEEP_TIME = 2
+
+# 通讯录显示的好友昵称
+FRIEND = "好友"
+# 指定截止时间
+TDATETIME = "1990年1月1日"
