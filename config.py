18082aa9f60fb18d3478cb7c31d10689ed8b93fb
NanMu93 <mscboom@gmail.com>
2023-02-03 16:31:11 +0800

    Update config.py
    更新，可爬取图片和视频

--- config.py
+++ config.py
@@ -11,12 +11,19 @@ FLICK_START_Y = 300
 FLICK_START_X = 300
 FLICK_START_Y = 300
 
+# 模拟器或手机保存微信图片路径
+RP_DIRECTORY = '/storage/self/primary/Pictures/WeiXin/'  
+# 模拟器或手机保存微信视频路径
+RV_DIRECTORY = '/sdcard/DCIM/WeiXin/'
+# 本地保存数据的路径
+L_DIRECTORY = 'F:/SAVEPATH/'
 
-SCROLL_SLEEP_TIME = 2
+SCROLL_SLEEP_TIME = 0.5
 
 # 通讯录显示的好友昵称
-FRIEND = "好友"
-# 指定截止时间
+FRIEND = "好友昵称"
+# 指定截止日期
 TDATETIME = "1990年1月1日"
-# 导出Excel文件路径
-EXCEL_FILE = "F:/wechat.xlsx"
+
+# 指定开始爬取的日期
+STARTTIME = "2022年8月21日"
